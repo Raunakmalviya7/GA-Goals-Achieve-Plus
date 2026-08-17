4ab89b21495a99a36da0d22f85f6ce3f662caa20
Raunakmalviya7 <malviyaraunak7@gmail.com>
2026-08-17 16:34:53 +0530

    Initialize settings.gradle.kts with project configuration

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "GA-Goals-Achieve-Plus"
+include(":app")
